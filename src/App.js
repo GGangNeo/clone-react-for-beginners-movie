d239d673426d5d4e4440d39f1ea12d9ab3b93d07
GGangNeo <107779482+GGangNeo@users.noreply.github.com>
2022-07-26 23:34:09 +0900

    todo app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import { useEffect, useState } from 'react';
-import style from './App.module.css';
-import Button from './Button';
-import Greeting from './Greeting';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const [show, setShow] = useState(false);
-  const onClick = () => setCount((current) => current + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  const onHide = () => setShow((current) => !current);
-  console.log('It will keep running');
-
-  useEffect(() => {
-    console.log('It runs only once.');
-  }, []);
-
-  useEffect(() => {
-    console.log('It runs when keyword changed');
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log('It runs when count changed');
-  }, [count]);
-
-  useEffect(() => {
-    console.log('It runs when count and keyword changed');
-  }, [count, keyword]);
-
+  const [toDo, setTodo] = useState('');
+  const [toDos, setTodos] = useState([]);
+  const onChagne = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') return;
+    setTodos((current) => [toDo, ...current]);
+    setTodo('');
+  };
   return (
-    <div className={style.title}>
-      {show ? <Greeting /> : null}
-      <input value={keyword} placeholder="Search" onChange={onChange}></input>
-      {show ? <h1> {count}</h1> : null}
-      <Button text={'Click me'} onClick={onClick} />
-      <Button text={show ? 'Hide' : 'Show'} onClick={onHide} />
+    <div>
+      <h1> ToDos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChagne}
+          placeholder="Enter Todo"
+          type="text"
+        ></input>
+        <button> Save </button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
 }
