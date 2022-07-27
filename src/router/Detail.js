14ffb30c4775fb06a0d6b56ba14ca04d7599737c
GGangNeo <107779482+GGangNeo@users.noreply.github.com>
2022-07-27 18:09:23 +0900

    clean up

--- src/router/Detail.js
+++ src/router/Detail.js
@@ -1,15 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 const Detail = () => {
+  const [detail, setDetail] = useState([]);
   const { id } = useParams();
-  const getMovie = async () => {
+
+  const getDetail = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setDetail(json.data.movie);
   };
+
   useEffect(() => {
-    getMovie();
+    getDetail();
   }, []);
 
   return <h3> Details </h3>;
