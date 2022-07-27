f48f7bc820d040ba141a9ad0e47d5d770e7a1949
GGangNeo <107779482+GGangNeo@users.noreply.github.com>
2022-07-27 14:49:25 +0900

    movie app part 1 clean up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { useEffect, useState } from 'react';
+import Movie from './Movie';
 
 function App() {
   const [movie, setMovie] = useState([]);
   const [load, setLoad] = useState(true);
   const getMovies = async () => {
-    // const response = await fetch(
-    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    // );
-    // const json = await response.json();
-
     const json = await (
       await fetch(
         `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
@@ -19,15 +15,6 @@ function App() {
   };
 
   useEffect(() => {
-    // fetch(
-    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    // )
-    //   .then((res) => res.json())
-    //   .then((res) => {
-    //     console.log(res.data.movies);
-    //     setMovie(res.data.movies);
-    //     setLoad(false);
-    //   });
     getMovies();
   }, []);
   return (
@@ -37,17 +24,13 @@ function App() {
       ) : (
         <div>
           {movie.map((item) => (
-            <div key={item.id}>
-              {' '}
-              <img src={item.medium_cover_image} />
-              <h2>{item.title}</h2>
-              <p>{item.summary}</p>
-              <ul>
-                {item.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
+            <Movie
+              key={item.id}
+              coverImage={item.medium_cover_image}
+              title={item.title}
+              summary={item.summary}
+              genres={item.genres}
+            />
           ))}
         </div>
       )}
