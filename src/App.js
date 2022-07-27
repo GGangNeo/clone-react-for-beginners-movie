713e6b84820cd19aebf8389359d63bf8f3e8fc5a
GGangNeo <107779482+GGangNeo@users.noreply.github.com>
2022-07-27 13:34:41 +0900

    movie app part 1
    get information about movies

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [coins, setCoins] = useState([]);
+  const [movie, setMovie] = useState([]);
   const [load, setLoad] = useState(true);
+  const getMovies = async () => {
+    // const response = await fetch(
+    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    // );
+    // const json = await response.json();
+
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovie(json.data.movies);
+    setLoad(false);
+  };
+
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoad(false);
-        console.log(json);
-      });
+    // fetch(
+    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    // )
+    //   .then((res) => res.json())
+    //   .then((res) => {
+    //     console.log(res.data.movies);
+    //     setMovie(res.data.movies);
+    //     setLoad(false);
+    //   });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1> Coin Tracker ({coins.length}) </h1>
-      {load ? <strong> Loading..</strong> : null}
-      <ul>
-        {load
-          ? null
-          : coins.map((item) => (
-              <li key={item.id}>
-                {item.name} ({item.symbol}) ${item.quotes.USD.price}
-              </li>
-            ))}
-      </ul>
+      {load ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movie.map((item) => (
+            <div key={item.id}>
+              {' '}
+              <img src={item.medium_cover_image} />
+              <h2>{item.title}</h2>
+              <p>{item.summary}</p>
+              <ul>
+                {item.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
